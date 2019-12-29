e417ae72af8dea18982fcfb8ab69adda1836c43c
ipninichuck <ipninichuck@gmail.com>
2019-12-29 12:29:59 -0800

    Paramertized the configuration path for filebeat in order to handle multiple instances.

--- install-fb-module.sh
+++ install-fb-module.sh
@@ -4,15 +4,18 @@
 #Author Ivan Ninichuck
 #Version 12292019
 
+
 #Designate which modules to enable
 echo "Please enter which modules you would like to enable. Use a space between modules names."
 echo
 read modules
 echo
+echo "Please provide the configuration path."
+read config 
 echo "Thank You"
 
 #Activate the designated modules
-filebeat modules enable $modules
+filebeat --path.config $config modules enable $modules
 echo
-echo "$modules have been activated. Please alter configurations found at /etc/filebeat/modules.d."
+echo "$modules have been activated. Please alter configurations found at $config/modules.d."
 
