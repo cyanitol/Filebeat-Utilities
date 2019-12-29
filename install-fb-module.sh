801d7e26cd0fb1311d71bbed514425ccdc4a70d5
ipninichuck <ipninichuck@gmail.com>
2019-12-28 20:35:07 -0800

    Added a script that can activate any given modules.

---
+++ install-fb-module.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Description Enable Filebeat Module
+#Author Ivan Ninichuck
+#Version 12292019
+
+#Designate which modules to enable
+echo "Please enter which modules you would like to enable. Use a space between modules names."
+echo
+read modules
+echo
+echo "Thank You"
+
+#Activate the designated modules
+filebeat modules enable $modules
+echo
+echo "$modules have been activated. Please alter configurations found at /etc/filebeat/modules.d."
+
